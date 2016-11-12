2efd2030ae74c4b4fe6dd52457e3fb6794bb07aa
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2016-11-12 14:12:37 +0000

    Add basic emain

---
+++ src/emain.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <e-lib.h>
+
+#include "shared.h"
+
+
+
+int main(void) {
+    int* state = (int*) OFFSET_STT;
+    
+
+
+}
