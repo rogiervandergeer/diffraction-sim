340b13b7880c12e4685ce8b8e37313fb48d734b2
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2016-11-12 09:22:20 +0000

    Add build script.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+ESDK=${EPIPHANY_HOME}
+ELIBS="-L ${ESDK}/tools/host/lib"
+EINCS="-I ${ESDK}/tools/host/include"
+ELDF=${ESDK}/bsps/current/internal.ldf
+
+# Build host app
+gcc main.c -o bin/main.elf ${EINCS} ${ELIBS} -le-hal -le-loader -lpthread
+
+# Build device app
+e-gcc -T ${ELDF} -O0 emain.c -o emain.elf -le-lib -lm
