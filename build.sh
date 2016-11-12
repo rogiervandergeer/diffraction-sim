0b61623846184b86418e9ee3ed718bf9d0619b81
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2016-11-12 09:23:58 +0000

    Add dir creation

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ ELIBS="-L ${ESDK}/tools/host/lib"
 EINCS="-I ${ESDK}/tools/host/include"
 ELDF=${ESDK}/bsps/current/internal.ldf
 
+# Create binary dir
+if [ ! -d bin ]; then
+  mkdir -p bin
+fi
+
 # Build host app
 gcc main.c -o bin/main.elf ${EINCS} ${ELIBS} -le-hal -le-loader -lpthread
 
