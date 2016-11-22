42ddf06c18183ada193e5702b4bd7abd97699cda
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2016-11-22 10:12:02 +0000

    Update benchmark and commit results

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -5,6 +5,11 @@ import math
 import subprocess
 import time
 
+def read():
+    subprocess.check_output(["git", "checkout", "./benchmark.txt"])
+    print('Previous result:')
+    with open('benchmark.txt') as bf:
+        print(bf.read())
 
 def benchmark(opt, benchmark):
     subprocess.check_output(["./build.sh", "-O{opt}".format(opt=opt)])
@@ -15,14 +20,19 @@ def benchmark(opt, benchmark):
     return (datetime.datetime.now()-start).total_seconds()
 
 def main():
-    
-    for opt in range(4):
-        timings = [ ]
-        for bm in range(3):
-            score = benchmark(opt, bm)
-            n = 10240000 if bm > 0 else 640000 
-            timings.append(score/(n*math.pi*0.25))
-        print("{opt}:".format(opt=opt), timings)
+    read()
+    print('\nCurrent result:')
+    with open('benchmark.txt', 'w') as bf:
+        for opt in range(4):
+            timings = [ ]
+            for bm in range(3):
+                score = benchmark(opt, bm)
+                n = 10240000 if bm > 0 else 640000 
+                timings.append(score/(n*math.pi*0.25))
+            print("%s: %.4g, %.4g, %.4g" % 
+                    (opt, timings[0], timings[1], timings[2]))
+            bf.write("%s: %.4g, %.4g, %.4g" % 
+                    (opt, timings[0], timings[1], timings[2]))
 
 main()
 
