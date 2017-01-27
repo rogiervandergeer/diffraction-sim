1799c50fb742162e6e45ab651995de9541b6d9be
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-01-27 15:33:11 +0100

    Add create_folders and run

--- control/utils.py
+++ control/utils.py
@@ -1,4 +1,7 @@
 from subprocess import check_output
+from datetime import datetime
+from os import mkdir
+from os.path import isdir
 
 
 def build(optimisation=0):
@@ -11,6 +14,21 @@ def build(optimisation=0):
     check_output(['./build.sh', '-O{opt}'.format(opt=optimisation)])
 
 
-def run(definition_file, id):
-    pass
+def create_folders():
+    """Create the 'sources', 'plates' and 'output' folders."""
+    for folder in ('sources/', 'plates/', 'output/'):
+        if not isdir(folder):
+            mkdir(folder)
 
+
+def run(definition_file, output_file, definition_id=None):
+    start = datetime.now()
+    command = ['./bin/main.elf', definition_file, output_file]
+    if type(definition_id) == int:
+        command.append(str(definition_id))
+    elif type(definition_id) == tuple:
+        command.append(str(definition_id[0]))
+        command.append(str(definition_id[1]))
+    check_output(command)
+    end = datetime.now()
+    return (end-start).total_seconds()
